82c37e4328cb438419e77a0d1300980d447864da
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-11-23 20:42:10 -0800

    Update Middle-earth.js

--- Middle-earth.js
+++ Middle-earth.js
@@ -67,3 +67,12 @@ function goodVsEvil(good, evil) {
 }
 
 // or
+
+function goodVsEvil(good, evil) {
+  let worth = [ [1, 2, 3, 3, 4, 10], [1, 2, 2, 2, 3, 5, 10] ];
+  let good = good.split(' ').reduce((s,v,i) => s + worth[0][i] * v, 0);
+  let evil = evil.split(' ').reduce((s,v,i) => s + worth[1][i] * v, 0);
+  if (good > evil) return "Battle Result: Good triumphs over Evil";
+  else if (evil > good) return "Battle Result: Evil eradicates all trace of Good";
+  else return "Battle Result: No victor on this battle field";
+}
